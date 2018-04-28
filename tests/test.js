10c2b2c43d4cab74acfbf624f89e0b73b9d60e3d
Mike Donnelly <sqldonnelly@gmail.com>
2018-04-28 16:32:08 +0900

    Set up environment with mocha, knex, and node

---
+++ tests/test.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+const { expect } = require("chai");
+//const config = require("../config");
+//const knex = require("knex")(config.db);
+//const db = require("../services/db")(config.db);
+//const Promise = require("bluebird");
+
+describe("tests", () => {});
