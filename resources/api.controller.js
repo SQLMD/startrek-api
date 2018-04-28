f7666321635a1e530ab8054de263bb6089180cba
Mike Donnelly <sqldonnelly@gmail.com>
2018-04-28 17:48:02 +0900

    Add route for startrek/episodes

--- resources/api.controller.js
+++ resources/api.controller.js
@@ -3,12 +3,22 @@ const FAIL = 400;
 //const SERVER_ERROR = 500;
 
 const render = (res, code, page) => {
-  // send a response
   res.status(code).render(page);
 };
+const send = (res, code, data, json = true) => {
+  res.status(code).send(json ? JSON.stringify(data) : data);
+};
+
+const episodeData = [
+  { id: 1, title: "The Man Trap", airDate: "1966-09-08" },
+  { id: 2, title: "Charlie X", airDate: "1966-09-15" }
+];
 
 module.exports = {
   home(req, res) {
     render(res, OK, "home.ejs");
+  },
+  allEpisodes(req, res) {
+    send(res, OK, episodeData, true);
   }
 };
--- resources/api.router.js
+++ resources/api.router.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const controller = require("./api.controller.js");
 
+router.get("/startrek", controller.home);
 router.get("/", controller.home);
+router.get("/startrek/episodes", controller.allEpisodes);
 
 module.exports = router;
